d07b1113632487ee80cde598895d75b0f75ed7f2
akinsola <thomas.lawanson@gmail.com>
2020-05-15 16:13:12 +0100

    update

--- src/Enum/ETableName.ts
+++ src/Enum/ETableName.ts
@@ -1,4 +1,5 @@
 export enum ETableName {
     VIDEOS = "videos",
-    COMMUNICATION = "screen_communication"
+    COMMUNICATION = "screen_communication",
+    SCREENS = 'screens'
 }
--- src/controllers/VideoController.ts
+++ src/controllers/VideoController.ts
@@ -1,50 +1,138 @@
 import { ApiGatewayManagementApi } from "aws-sdk";
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { ResponseService } from '../services/ResponseService';
+import { ResponseService } from "../services/ResponseService";
 import { IVideo } from "../interfaces/IVideo";
-import { DatabaseService } from '../services/DatabaseService';
+import { DatabaseService } from "../services/DatabaseService";
 import { EVideoType } from '../Enum/EVideoType';
-import { VideoFile } from '../models/VideoFile';
+import { VideoFile } from "../models/VideoFile";
 import { VideoStream } from "../models/VideoStream";
-import { ETableName } from '../Enum/ETableName';
-import { IAddVideoRequestBody } from '../interfaces/IRequest';
+import { ETableName } from "../Enum/ETableName";
+import {
+  IAddVideoRequestBody,
+  IUpdateVideoRequestBody
+} from "../interfaces/IRequest";
+import validator from "validator";
+
 export const addVideo: APIGatewayProxyHandler = async (event, context) => {
-    try {
-      let video: IVideo;
-      let db: DatabaseService;
-      const body: IAddVideoRequestBody = JSON.parse(event.body);
+  try {
+    let video: IVideo;
+    let db: DatabaseService;
+    const body: IAddVideoRequestBody = JSON.parse(event.body);
 
-      if(!body.title) {
-        throw new Error("Request is missing title");
-      }
-
-      if(!body.filename) {
-        throw new Error("Request is missing filename");
-      }
-
-      if(!body.type) {
-        throw new Error("Request is missing type");
-      }
-
-      switch(body.type) {
-        case EVideoType.FILE: 
-          video = new VideoFile(body.title, body.filename);
-          break;
-        case EVideoType.STREAM:
-          video = new VideoStream(body.title, body.filename);
-          break;
-        default:
-          break;
-      }
-
-      if(video) {
-        db = new DatabaseService(ETableName.VIDEOS);
-        await db.putItem(video)
-      }
-
-      return ResponseService.success(video)
-
-    } catch (error) {
-      return ResponseService.error(error.message, error.statusCode);
+    if (validator.isAlphanumeric(body.title) && !validator.isEmpty(body.title)) {
+      throw new Error("Request is missing title");
     }
-  };
+
+    if (validator.isAscii(body.filename)) {
+      throw new Error("Request is missing filename");
+    }
+
+    if (!body.type) {
+      throw new Error("Request is missing type");
+    }
+
+    switch (body.type) {
+      case EVideoType.FILE:
+        video = new VideoFile(body.title, body.filename);
+        break;
+      case EVideoType.STREAM:
+        video = new VideoStream(body.title, body.filename);
+        break;
+      default:
+        break;
+    }
+
+    if (video) {
+      db = new DatabaseService(ETableName.VIDEOS);
+      await db.putItem(video);
+    }
+
+    return ResponseService.success(video);
+  } catch (error) {
+    return ResponseService.error(error.message, error.statusCode);
+  }
+};
+
+export const getAllVideos: APIGatewayProxyHandler = async (event, context) => {
+  try {
+    let videos: IVideo[] = [];
+    let db: DatabaseService;
+
+    db = new DatabaseService(ETableName.VIDEOS);
+    let response = await db.getAllItems();
+    videos = response.Items;
+    return ResponseService.success(videos);
+  } catch (error) {
+    return ResponseService.error(error.message, error.statusCode);
+  }
+};
+
+export const updateVideo: APIGatewayProxyHandler = async (event, context) => {
+  try {
+    const body: IUpdateVideoRequestBody = JSON.parse(event.body);
+    if (!body.id) {
+      throw new Error("Request is missing id");
+    }
+    if (!body.title) {
+      throw new Error("Request is missing title");
+    }
+
+    if (!body.filename) {
+      throw new Error("Request is missing filename");
+    }
+
+    if (!body.type) {
+      throw new Error("Request is missing type");
+    }
+
+    let video: IVideo;
+
+    video = {
+      id: body.id,
+      filename: body.filename,
+      title: body.title,
+      type: body.type
+    };
+
+    let db: DatabaseService;
+
+    db = new DatabaseService(ETableName.VIDEOS);
+    await db.updateItem(video.id, {
+      filename: video.filename,
+      type: video.type,
+      title: video.title
+    });
+
+    return ResponseService.success(video);
+  } catch (error) {
+    return ResponseService.error(error.message, error.statusCode);
+  }
+};
+
+export const deleteVideo: APIGatewayProxyHandler = async (event, context) => {
+  try {
+    const body: IUpdateVideoRequestBody = JSON.parse(event.body);
+    if (!body.id) {
+      throw new Error("Request is missing id");
+    }
+    if (!body.title) {
+      throw new Error("Request is missing title");
+    }
+
+    if (!body.filename) {
+      throw new Error("Request is missing filename");
+    }
+
+    if (!body.type) {
+      throw new Error("Request is missing type");
+    }
+
+    let db: DatabaseService;
+
+    db = new DatabaseService(ETableName.VIDEOS);
+    await db.deleteItem(body.id);
+    return ResponseService.success(`${body.title} successfully deleted`);
+  } catch (error) {
+    return ResponseService.error(error.message, error.statusCode);
+  }
+};
